2f8e1cd79ce0e52917e477212790b4d93ace322a
Kevin Chen <kevinc@macOS-SOIFFOC-7.local>
2023-12-02 18:46:51 -0500

    encapulated logic

--- EmailHtmlMapper.js
+++ EmailHtmlMapper.js
@@ -1,34 +1,36 @@
 const fs = require('fs');
-import { jsonData } from './csvToJson';
 // Initialize an object to hold the results
 let associatedHTML = {};
 
-jsonData.forEach(item => {
-    const email = item.Email;
-
-    // Assuming you have a function to get the HTML content for each email
-    const htmlContent = fs.readFile('All mail Including Spam and Trash.txt')
-
-    // Regex to find the pattern
-    const regex_pattern = '${emailPattern}[\\s\\S]*?Verify your email';
-    const regex = new RegExp(regex_pattern);
-    const match = regex.exec(htmlContent);
-
+function mapData(jsonData) {
+    jsonData.forEach(item => {
+        const email = item.Email;
     
+        // Assuming you have a function to get the HTML content for each email
+        const htmlContent = fs.readFile('All mail Including Spam and Trash.txt')
+    
+        // Regex to find the pattern
+        const regex_pattern = '${emailPattern}[\\s\\S]*?Verify your email';
+        const regex = new RegExp(regex_pattern);
+        const match = regex.exec(htmlContent);
+    
+        
+    
+        if (match) {
+            associatedHTML[email] = match[0];
+        } else {
+            associatedHTML[email] = "No match found";
+        }
+    });
+}
 
-    if (match) {
-        associatedHTML[email] = match[0];
-    } else {
-        associatedHTML[email] = "No match found";
-    }
-});
-
-
-// Write results to a JSON file
-fs.writeFile('AssociatedHTML.json', JSON.stringify(associatedHTML, null, 2), (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log('Associated HTML saved to AssociatedHTML.json');
-});
+function exportToFile(outputPath, ) {
+    // Write results to a JSON file
+    fs.writeFile('AssociatedHTML.json', JSON.stringify(associatedHTML, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Associated HTML saved to AssociatedHTML.json');
+    });
+}
--- csvToJson.js
+++ csvToJson.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 const csv = require('csv-parse');
-const output = [];
 
-fs.createReadStream('Accounts.csv')
-    .pipe(csv.parse({ columns: true }))
-    .on('data', (data) => output.push(data))
-    .on('end', () => {
-        // Convert the output array to a JSON string
-        const json = JSON.stringify(output);
 
-        // Specify the path for the output file
-        const outputPath = 'Accounts.json';
-
-        // Write the JSON string to the file
-        fs.writeFile(outputPath, json, 'utf8', (err) => {
-            if (err) {
-                console.error('An error occurred:', err);
-                return;
-            }
-            console.log(`Saved JSON to ${outputPath}`);
-        });
-});
+function createJsonFile(inputPath, outputPath){
+    const output = [];
+    fs.createReadStream(inputPath)
+        .pipe(csv.parse({ columns: true }))
+        .on('data', (data) => output.push(data))
+        .on('end', () => {
+            // Convert the output array to a JSON string
+            const json = JSON.stringify(output);
+    
+            // Write the JSON string to the file
+            fs.writeFile(outputPath, json, 'utf8', (err) => {
+                if (err) {
+                    console.error('An error occurred:', err);
+                    return;
+                }
+                console.log(`Saved JSON to ${outputPath}`);
+            });
+    });
+}
 
 // Function to read and parse the JSON file
-function readJsonFile(filePath) {
+function createJsonObject(filePath) {
     try {
         // Read the file synchronously
         const jsonString = fs.readFileSync(filePath, 'utf8');
@@ -38,15 +38,14 @@ function readJsonFile(filePath) {
     }
 }
 
-// Use the function to read your JSON file
-const jsonFilePath = 'Accounts.json'; // Path to your JSON file
-const jsonData = readJsonFile(jsonFilePath);
-if (jsonData) {
-    console.log(jsonData[1]); // Accessing the second element in the JSON array
-}
+// // Use the function to read your JSON file
+// const jsonFilePath = 'Accounts.json'; // Path to your JSON file
+// const jsonData = readJsonFile(jsonFilePath);
+// if (jsonData) {
+//     console.log(jsonData[1]); // Accessing the second element in the JSON array
+// }
 
 module.exports = {
     createReadStream,
     readJsonFile,
-    jsonData
 }
